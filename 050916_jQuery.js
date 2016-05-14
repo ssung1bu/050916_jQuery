b195434f7e5646f5582f7db1c899164618c517a9
Sarah Sung <mssarahsung@gmail.com>
2016-05-14 13:01:48 -0400

    add CSS and modify JS and HTML

--- 050916_jQuery.js
+++ 050916_jQuery.js
@@ -1,5 +1,17 @@
 $(document).ready(
 	function(){
 		alert("The document is ready!");  
+$("ul li").hide();
+$("ul li").fadeIn(5000);
+$("#slide").slideUp(4000); //slideDown doesn't work for some reason
+$("h1").animate( {
+	opacity: 0.25,
+	width: "70%"
+	} , 2000 );
+}
+);
+$("#clickme").click(
+function(){
+$("#clickme").hide()
 }
 );
